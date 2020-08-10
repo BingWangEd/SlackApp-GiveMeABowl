1f6a8af5087a67b5d748ccbead2251e13eaf4c36
Bing Wang <bwang01@uchicago.edu>
2020-08-10 19:50:22 +0900

    session 2 - creates simple response to Slack channel's slash command

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const http = require('http');
 const { httpsGetRequest } = require('./src/httpsMethods');
+const { parse } = require('querystring');
 
 //create a server object:
-http.createServer(async (req, res) => {
-  const requestUrl = 'https://next.json-generator.com/api/json/get/VkM--CPgY';
-  const result = await httpsGetRequest(requestUrl);
-  console.log('result: ', result);
+const server = http.createServer(async (req, res) => {
+  if (req.method !== 'POST') return;
 
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-  
-}).listen(3000); //the server object listens on port 3000
+  collectRequestData(req, (result) => {
+    if (!result) return;
+    console.log('result: ', result);
+    // send response
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+  });
+}); //the server object listens on port 3000
+
+const collectRequestData = (request, callback) => {
+  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+  let result = null;
+  return new Promise((resolve, reject) => {
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+      // parse and read data
+      let body = [];
+      request.on('data', chunk => {
+        body.push(chunk);
+      });
+      request.on('end', () => {
+        // request content type: application/x-www-form-urlencoded
+        body = Buffer.concat(body).toString();
+        result = callback(parse(body));
+        resolve(result);
+      });
+    } else {
+      reject(callback(null)); // call error
+    }
+  });
+}
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
